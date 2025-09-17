331bb1d4ae37f0d7858c90323e62a0b94b1b9ee0
belabastos-art <mb10491@nyu.edu>
2025-09-17 10:25:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
-function playSound() {
-    const audio = new (window.AudioContext || window.webkitAudioContext)();
-    const beep = audio.createOscillator();
-    beep.frequency.value = 800;
-    beep.connect(audio.destination);
-    beep.start();
-    beep.stop(audio.currentTime + 0.2);
-}
-    
+
 function goToScene2() {
-    playSound();
+
     document.getElementById('img1').classList.add('hidden');
     document.getElementById('img2').classList.remove('hidden');
+    
     document.getElementById('scene1').classList.add('hidden');
     document.getElementById('scene2').classList.remove('hidden');
 }
 
 function goToScene3() {
-    playSound();
+    
     document.getElementById('scene2').classList.add('hidden');
     document.getElementById('scene3').classList.remove('hidden');
 }
